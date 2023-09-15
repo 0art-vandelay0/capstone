9e8c31eabfe4e616be62e6be29493f49235c1d5b
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-09-15 15:52:16 -0700

    Add pep8 to formatter

---
+++ flask-server/Twilio.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+
+def send_sms(body, to, from_):
+    account_sid = "your_twilio_account_sid"
+    auth_token = "your_twilio_auth_token"
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=body,
+        from_=from_,
+        to=to
+    )
+    return message.sid
