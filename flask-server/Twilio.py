f4f3dcaa2560c1000af8f5fdfab1266dae4b1120
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-09-15 16:12:23 -0700

    Add Twilio BE logic

--- flask-server/Twilio.py
+++ flask-server/Twilio.py
@@ -5,9 +5,11 @@ def send_sms(body, to, from_):
     account_sid = "your_twilio_account_sid"
     auth_token = "your_twilio_auth_token"
     client = Client(account_sid, auth_token)
+
     message = client.messages.create(
-        body=body,
+
         from_=from_,
+        body='This is the code for the bathroom',
         to=to
     )
     return message.sid
