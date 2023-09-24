22364566109694dd049959bed0818e5e48fcc29e
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-09-23 22:03:40 -0700

    Add test code to start moving page format of components

--- client/src/MapControl.js
+++ client/src/MapControl.js
@@ -14,3 +14,28 @@ function MapControl() {
 }
 
 export default MapControl;
+
+
+// Compare component page organization
+
+// import React, { useState } from 'react';
+// import Data from './Data';
+// import MyMapComponent from './MyMapComponent';
+
+// function MapControl() {
+//     const [selectedBathroom, setSelectedBathroom] = useState(null);
+
+//     return (
+//         <div style={{ display: 'flex', flexDirection: 'row' }}>
+//             <div style={{ flex: 1 }}>
+//                 <Data setSelectedBathroom={setSelectedBathroom} />
+//             </div>
+//             <div style={{ flex: 1 }}>
+//                 <MyMapComponent selectedBathroom={selectedBathroom} setSelectedBathroom={setSelectedBathroom} />
+//             </div>
+//         </div>
+//     );
+// }
+
+
+// export default MapControl;
