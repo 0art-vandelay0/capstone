ee1df2b5eabffa168da9b2b031a6ae48c2834a06
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-09-24 18:16:02 -0700

    Adjust layout

--- client/src/MapControl.js
+++ client/src/MapControl.js
@@ -1,23 +1,3 @@
-// import React, { useState } from 'react';
-// import Data from './Data';
-// import MyMapComponent from './MyMapComponent';
-
-// function MapControl() {
-//     const [selectedBathroom, setSelectedBathroom] = useState(null);
-
-//     return (
-//         <div>
-//             <Data setSelectedBathroom={setSelectedBathroom} />
-//             <MyMapComponent selectedBathroom={selectedBathroom} setSelectedBathroom={setSelectedBathroom} />
-//         </div>
-//     );
-// }
-
-// export default MapControl;
-
-
-// Compare component page organization
-
 import React, { useState } from 'react';
 import Data from './Data';
 import MyMapComponent from './MyMapComponent';
