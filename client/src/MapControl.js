a2453bed4a647d4f12fa10f6b6724646797f7ad9
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-09-24 18:57:57 -0700

    Rearrange to move details

--- client/src/MapControl.js
+++ client/src/MapControl.js
@@ -1,21 +1,49 @@
+// import React, { useState } from 'react';
+// import Data from './Data';
+// import MyMapComponent from './MyMapComponent';
+
+// function MapControl() {
+//     const [selectedBathroom, setSelectedBathroom] = useState(null);
+
+//     return (
+//         <div style={{ display: 'flex', flexDirection: 'row' }}>
+//         <div style={{ flex: 1 }}>
+//             <MyMapComponent selectedBathroom={selectedBathroom} setSelectedBathroom={setSelectedBathroom} />
+//         </div>
+//             <div id="dataComp" style={{ flex: 1 }}>
+//                 <Data setSelectedBathroom={setSelectedBathroom} />
+//             </div>
+//         </div>
+//     );
+// }
+
+
+// export default MapControl;
+
+
 import React, { useState } from 'react';
 import Data from './Data';
 import MyMapComponent from './MyMapComponent';
+import BathroomDetails from './BathroomDetails';
 
 function MapControl() {
     const [selectedBathroom, setSelectedBathroom] = useState(null);
 
     return (
         <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <div style={{ flex: 1 }}>
-            <MyMapComponent selectedBathroom={selectedBathroom} setSelectedBathroom={setSelectedBathroom} />
-        </div>
-            <div id="dataComp" style={{ flex: 1 }}>
-                <Data setSelectedBathroom={setSelectedBathroom} />
+            <div style={{ flex: 2 }}>
+                <MyMapComponent selectedBathroom={selectedBathroom} setSelectedBathroom={setSelectedBathroom} />
+            </div>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+                <div id="dataComp">
+                    <Data setSelectedBathroom={setSelectedBathroom} />
+                </div>
+                <div id="detailsContainer">
+                    {selectedBathroom && <BathroomDetails details={selectedBathroom} />}
+                </div>
             </div>
         </div>
     );
 }
-
 
 export default MapControl;
